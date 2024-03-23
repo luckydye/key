48390e2f8d810b078518c495c78b00a34d481493
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-03-23 19:15:46 +0100

    Initial rust rewrite

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
 # key
 
 ```
-Command Line Interface to a local or remote keepass database.
+Command Line Interface to a local or remote keepass database
 
-Optional Environment Variables:
-  KEEPASSDB - url to the keepass database (examples: file:///bucket/path or s3://s3.example.com/bucket/path)
-  KEEPASSDB_KEYFILE - path to the keyfile for the keepass database
-  KEEPASSDB_PASSWORD - password for the keepass database
+Usage: key [OPTIONS] [COMMAND]
 
-Usage:
-  key [command]
+Commands:
+  list  List all entries of the database
+  get   Get a specific entry from the database
+  help  Print this message or the help of the given subcommand(s)
 
-Available Commands:
-  completion  Generate the autocompletion script for the specified shell
-  get         Get an entry from the database
-  help        Help about any command
-  list        List all entries in the database
-
-Flags:
-  -h, --help   help for key
-
-Use "key [command] --help" for more information about a command.
+Options:
+  -d, --debug...
+  -k, --keyfile <KEYFILE>  Provide a keyfile
+  -h, --help               Print help
+  -V, --version            Print version
 ```
---
+++ src/bin/key.rs
@@ -0,0 +1,160 @@
+use anyhow::Result;
+use clap::{CommandFactory, Parser, Subcommand};
+use demand::Input;
+use keepass::{db::NodeRef, Database, DatabaseKey};
+use log::debug;
+use minio::s3::{args::ObjectConditionalReadArgs, client::Client, http::BaseUrl};
+use std::{env, fs::File};
+use url::Url;
+
+/// Command Line Interface to a local or remote keepass database.
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Provide a keyfile
+    #[arg(short = 'k', long)]
+    keyfile: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List all entries of the database
+    List {},
+
+    /// Get a specific entry from the database
+    Get {
+        /// Name of entry
+        name: String,
+    },
+}
+
+fn read_password() -> String {
+    let t = Input::new("Password for the database.")
+        .placeholder("Password")
+        .prompt("Password: ");
+
+    t.run().expect("error running input")
+}
+
+fn get_database_key(password: Option<String>, keypath: Option<String>) -> Result<DatabaseKey> {
+    let mut key = DatabaseKey::new();
+
+    if let Some(keypath) = keypath {
+        key = key.with_keyfile(&mut File::open(keypath)?)?;
+    }
+
+    if let Some(password) = password {
+        key = key.with_password(password.as_str())
+    } else {
+        key = key.with_password(read_password().as_str());
+    }
+
+    Ok(key)
+}
+
+async fn get_database(
+    dburl: &str,
+    keypath: Option<String>,
+    password: Option<String>,
+) -> Result<Database> {
+    let dburl_parsed = Url::parse(dburl)?;
+
+    let schema = dburl_parsed.scheme();
+
+    let source = match schema {
+        "file" => {
+            let p = dburl_parsed.path();
+            let f = File::open(p)?;
+            Ok(f)
+        }
+        "s3" => {
+            // let static_provider = StaticProvider::new(
+            //     "Q3AM3UQ867SPQQA43P2F",
+            //     "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+            //     None,
+            // );
+
+            let base_url: BaseUrl = dburl_parsed.host_str().unwrap().parse::<BaseUrl>()?;
+            let client = Client::new(base_url, None, None, None).unwrap();
+
+            let bucket_and_path = dburl_parsed.path()[1..].split_once('/');
+            let bucket = bucket_and_path.unwrap().0;
+            let object_path = bucket_and_path.unwrap().1;
+
+            debug!("bucket={}  object={}", bucket, object_path);
+
+            let args = &ObjectConditionalReadArgs::new(bucket, object_path).unwrap();
+            let obj = client.get_object(args).await;
+
+            debug!("{:?}", obj);
+
+            Err(anyhow::format_err!("S3 not supported yet"))
+        }
+        _ => Err(anyhow::format_err!("Unsupported schema \"{}\"", schema)),
+    };
+
+    let key = get_database_key(password, keypath)?;
+
+    if let Err(e) = source {
+        return Err(e);
+    }
+
+    let db = Database::open(&mut source.unwrap(), key)?;
+
+    Ok(db)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+
+    let cli = Cli::parse();
+
+    let KEEPASSDB: Option<String> = env::var("KEEPASSDB").ok();
+    let KEEPASSDB_KEYFILE: Option<String> = env::var("KEEPASSDB_KEYFILE").ok();
+    let KEEPASSDB_PASSWORD: Option<String> = env::var("KEEPASSDB_PASSWORD").ok();
+
+    if KEEPASSDB.is_none() {
+        return Err(anyhow::format_err!("No database url provided."));
+    }
+
+    debug!("db url {:?}", KEEPASSDB);
+
+    match &cli.command {
+        Some(Commands::List {}) => {
+            let db = get_database(
+                KEEPASSDB.unwrap().as_str(),
+                KEEPASSDB_KEYFILE,
+                KEEPASSDB_PASSWORD,
+            )
+            .await?;
+
+            println!("{:?}", db);
+
+            Ok(())
+        }
+        Some(Commands::Get { name }) => {
+            let db = get_database(
+                KEEPASSDB.unwrap().as_str(),
+                KEEPASSDB_KEYFILE,
+                KEEPASSDB_PASSWORD,
+            )
+            .await?;
+
+            if let Some(NodeRef::Entry(e)) = db.root.get(&["GITLAB_ACCESS_TOKEN"]) {
+                println!("{}", e.get_password().unwrap().to_string());
+            }
+
+            Ok(())
+        }
+        None => {
+            Cli::command().print_help()?;
+
+            println!("No command provided.");
+            Ok(())
+        }
+    }
+}
